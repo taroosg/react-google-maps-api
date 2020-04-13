0aa17449a78de1ed4eb412e0b216f6429a5cf359
taroosg <macchinetta.xyz@gmail.com>
2020-04-14 01:41:06 +0900

    fix

--- packages/react-google-maps-api-gatsby-example/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => (
-  <footer className='footer'>
-    <div className='text-secondary small'>2019 React Google Maps API</div>
-  </footer>
-)
-
-export default Footer
